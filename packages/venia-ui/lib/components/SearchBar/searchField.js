01ee67bef16e395aad171b71b29fb4543c179284
Yeremac <yeremac@MIQOs-iMac.local>
2019-10-29 17:23:09 +0400

    Worked on SearchField Component

--- packages/venia-ui/lib/components/SearchBar/searchField.js
+++ packages/venia-ui/lib/components/SearchBar/searchField.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { func, shape, string } from 'prop-types';
-import { Search as SearchIcon, X as ClearIcon } from 'react-feather';
+import { X as ClearIcon } from 'react-feather';
 import { useSearchField } from '@magento/peregrine/lib/talons/SearchBar';
+import defaultClasses from './searchField.css';
+import { mergeClasses } from '../../classify';
 
 import Icon from '../Icon';
 import TextInput from '../TextInput';
 import Trigger from '../Trigger';
 
 const clearIcon = <Icon src={ClearIcon} size={18} />;
-const searchIcon = <Icon src={SearchIcon} size={18} />;
 
 const SearchField = props => {
+    const classes = mergeClasses(defaultClasses, props.classes);
     const { location, onChange, onFocus } = props;
     const talonProps = useSearchField({ location, onChange });
     const { resetForm, value } = talonProps;
@@ -21,11 +23,12 @@ const SearchField = props => {
 
     return (
         <TextInput
-            after={resetButton}
-            before={searchIcon}
+            after={<span className={classes.iconSearch}/>}
+            before={resetButton}
             field="search_query"
             onFocus={onFocus}
             onValueChange={onChange}
+            placeholder="Search here..."
         />
     );
 };
