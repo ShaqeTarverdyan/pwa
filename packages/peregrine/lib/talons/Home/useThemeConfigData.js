be2348ddf63549e94446459b8c718914383d68e9
Yeremac <yeremac@MIQOs-iMac.local>
2019-11-07 13:20:41 +0400

    Create config data hook for Home Compnent

--- packages/peregrine/lib/talons/Home/useThemeConfigData.js
+++ packages/peregrine/lib/talons/Home/useThemeConfigData.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 /**
@@ -15,10 +15,20 @@ export const useThemeConfigData = props => {
         }
     }, [error]);
 
+    const visibleBrand = useMemo(() => {
+        const brandCount = data && data.storeConfig && data.storeConfig.pwa_homepage_brands_visible;
+        const splited = brandCount.split(',');
+        return {
+            desktop: splited[0] || 6,
+            mobile: splited[1] || 2
+        }
+    }, [data]);
     return {
         homePage_top_slider_id: data && data.storeConfig && data.storeConfig.pwa_homepage_top_slider,
         homepage_top_banner: data && data.storeConfig && data.storeConfig.pwa_homepage_top_banner,
         homepage_bottom_banner_left: data && data.storeConfig && data.storeConfig.pwa_homepage_bottom_banner_left,
         homepage_bottom_banner_right: data && data.storeConfig && data.storeConfig.pwa_homepage_bottom_banner_right,
+        pwa_homepage_show_brands: data && data.storeConfig && data.storeConfig.pwa_homepage_show_brands,
+        visibleBrand
     };
 };
